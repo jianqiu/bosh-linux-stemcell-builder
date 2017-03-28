c75825f484d43f46007d7538ebb2837e71e9ccb3
Matt Cui <mattcui@gmail.com>
2017-03-28 17:57:29 +0800

    debug

--- ci/tasks/make-hvm-version.sh
+++ ci/tasks/make-hvm-version.sh
@@ -5,4 +5,4 @@ set -e
 CANDIDATE_BUILD_NUMBER=$( cat version/number | sed 's/\.0$//;s/\.0$//' )
 CANDIDATE_BUILD_NUMBER=${CANDIDATE_BUILD_NUMBER}-hvm
 
-echo "${CANDIDATE_BUILD_NUMBER}" > "version/number-hvm"
+echo "${CANDIDATE_BUILD_NUMBER}" > "hvm-version-info/hvm-version"
